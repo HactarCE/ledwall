e5a56e87fdffec744045c3729f052b5a89362aef
HactarCE <6060305+HactarCE@users.noreply.github.com>
2025-10-25 18:08:47 -0400

    Compile on raspi

--- update.sh
+++ update.sh
@@ -3,8 +3,15 @@
 BIN_NAME=ledwall_rpi
 
 set -e
-cross build --release --target armv7-unknown-linux-musleabihf --bin "$BIN_NAME"
+
+echo "Copying files to raspi ..."
+rsync --exclude .git --exclude target --recursive --delete . pi:/tmp/ledwall
+echo "Building on raspi ..."
+ssh pi "cd /tmp/ledwall && CARGO_TARGET_DIR=~/ledwall_target_cache CARGO_TERM_COLOR=always CARGO_TERM_PROGRESS_WHEN=always CARGO_TERM_PROGRESS_WIDTH=20 ~/.cargo/bin/cargo build -p ledwall_rpi"
 echo "Build successful!"
+
+# cross build --release --target armv7-unknown-linux-musleabihf --bin "$BIN_NAME"
+# echo "Build successful!"
 
 echo "Killing old process '$BIN_NAME' ..."
 set +e
@@ -12,7 +19,8 @@ ssh pi "sudo pkill '$BIN_NAME'"
 set -e
 
 echo "Copying new file ..."
-scp target/armv7-unknown-linux-musleabihf/release/ledwall_rpi pi:~
+ssh pi 'cp ~/ledwall_target_cache/debug/ledwall_rpi ~'
+# scp target/armv7-unknown-linux-musleabihf/release/ledwall_rpi pi:~
 
 echo "Running process '$BIN_NAME' ..."
 ssh pi 'sudo nohup ~/'"$BIN_NAME"' >/dev/null 2>&1 </dev/null & disown'
